69447b758c500b0200aef64ed350442e6f69e393
Rahul <rahulwp24@gmail.com>
2024-09-19 20:42:44 +0600

    2nd problm solved

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ btnLogin.addEventListener("click", function (event) {
 
   // condison---
   if (phoneNumber === "1234" && pinNumber === "Harekrishna") {
-    window.location.href = "./home.html";
+    window.location.href = "home.html";
   } else {
     alert("tumi rang");
   }
